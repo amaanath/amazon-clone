65833a8e95e77e42426ff87135912e2983f49089
Amaanath Mumtaz <amaanathmumtaz@gmail.com>
2021-01-05 02:10:27 +0530

    Coded Product.js Component

--- src/Product.js
+++ src/Product.js
@@ -1,10 +1,32 @@
 import React from 'react'
 import "./Product.css"
 
-export default function Product() {
+export default function Product({id, title, image, price, rating}) {
     return (
         <div className="product">
             
+            <div className="product__info">
+                <p>{title}</p>
+
+                <p className="product__price">
+                    <small>$</small>
+                    <strong>{price}</strong>
+                </p>
+
+                <div className="product__rating">
+                    {Array(rating)
+                        .fill()
+                        .map((_, i) => (
+                        <p>🌟</p>
+                        ))}
+                </div>
+
+                <img src={image} alt="" />
+
+                <button>Add to Basket</button>
+
+            </div>
+
         </div>
     )
 }
